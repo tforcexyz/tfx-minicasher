60597afcd676763168ba0ffd78fd5a218b15f5d2
Tung Nguyen <2715595+tungnguyen17@users.noreply.github.com>
2020-11-03 19:29:35 +0700

    Initial database migration | TFX-112 [MINICASHER] Bootstrap accounting domains

---
+++ src/TfxMiniCasher.Data.Migrations/scripts/Accounting/mysql/00000000000001_Initialize.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(95) NOT NULL,
+    `ProductVersion` varchar(32) NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+);
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('00000000000001_Initialize', '2.2.6-servicing-10079');
+
---
+++ src/TfxMiniCasher.Data.Migrations/scripts/Accounting/sqlite/00000000000001_Initialize.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
+    "ProductVersion" TEXT NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('00000000000001_Initialize', '2.2.6-servicing-10079');
+
