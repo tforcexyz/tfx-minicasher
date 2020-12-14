6e2520c75186cc9a3b513d65b2f0f0d2feacb51a
Tung Nguyen <2715595+tungnguyen17@users.noreply.github.com>
2020-12-14 23:45:11 +0700

    Setup icon pack | TFX-115 [MINICASHER] Create empty layout for ngx-admin

--- src/TfxMiniCasherWwwUI/src/app/app.component.ts
+++ src/TfxMiniCasherWwwUI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NbIconLibraries } from '@nebular/theme';
 import { NbMenuItem } from '@nebular/theme';
 
 @Component({
@@ -10,27 +11,67 @@ export class AppComponent {
 
   title = 'Angular10 Webpack';
 
+  constructor(private iconLibraries: NbIconLibraries) {
+    this.iconLibraries.registerFontPack('fa', { packClass: 'fa', iconClassPrefix: 'fa' });
+    this.iconLibraries.registerFontPack('fas', { packClass: 'fas', iconClassPrefix: 'fa' });
+    this.iconLibraries.registerFontPack('fad', { packClass: 'fad', iconClassPrefix: 'fa' });
+    this.iconLibraries.registerFontPack('fal', { packClass: 'fal', iconClassPrefix: 'fa' });
+    this.iconLibraries.registerFontPack('far', { packClass: 'far', iconClassPrefix: 'fa' });
+  }
+
   private menus: NbMenuItem[] = [
     {
       title: 'Dashboard',
       link: '.',
-      icon: 'home-outline',
+      icon: { icon: 'chart-pie', pack: 'far' },
       home: true,
     },
     {
-      title: 'GROUP',
+      title: 'TRANSACTION',
       group: true,
     },
     {
-      title: 'Level 1',
+      title: 'Transaction',
       link: '.',
-      icon: 'list',
-      children: [
-        {
-          title: 'Level 1.1',
-          link: '.',
-        },
-      ]
+      icon: { icon: 'exchange', pack: 'far' },
+    },
+    {
+      title: 'Recurring transaction',
+      link: '.',
+      icon: { icon: 'alarm-clock', pack: 'far' },
+    },
+    {
+      title: 'MASTER DATA',
+      group: true,
+    },
+    {
+      title: 'Account',
+      link: '.',
+      icon: { icon: 'landmark', pack: 'far' },
+    },
+    {
+      title: 'Category',
+      link: '.',
+      icon: { icon: 'list', pack: 'far' },
+    },
+    {
+      title: 'REPORT',
+      group: true,
+    },
+    {
+      title: 'Balance',
+      link: '.',
+      icon: { icon: 'balance-scale', pack: 'far' },
+    },
+    {
+      title: 'Expense',
+      link: '.',
+      icon: { icon: 'chart-line-down', pack: 'far' },
+    },
+    {
+      title: 'Income',
+      link: '.',
+      icon: { icon: 'chart-line', pack: 'far' },
     },
   ];
 }
